882ed74a2c3a7e6643b6e97b232aa8279d42f039
VM <VM@VIRTUALMACHINE>
2016-09-13 16:12:02 -0700

    initial commit

--- multiply/multiply.cpp
+++ multiply/multiply.cpp
@@ -2,10 +2,25 @@
 //
 
 #include "stdafx.h"
+#include "iostream"
+using namespace std;
 
-
-int main()
+int main(void)
 {
-    return 0;
-}
+	cout << "Multiplication Table with Nested Loop" << endl;
 
+
+	for (int c = 0; c < 11; c++)
+	{
+		cout << c << "|";
+		for (int i = 0; i < 11; i++)
+		{
+			cout << i * c << '\t';
+		}
+
+		cout << endl;
+	}
+
+	system("pause");
+	return 0;
+}
